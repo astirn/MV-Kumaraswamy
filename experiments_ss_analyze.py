e1e4c225a77cc3fc50e23bc1586d32cb3d76e91d
Andrew Stirn <andrew.stirn@gmail.com>
2019-10-24 08:46:52 -0400

    second 2 last run done

--- experiments_ss_analyze.py
+++ experiments_ss_analyze.py
@@ -10,7 +10,7 @@ from experiments_ss_run import methods, architectures
 
 # add parser arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('--data_set', type=str, default='mnist', help='data set name = {mnist, svhn_cropped}')
+parser.add_argument('--data_set', type=str, default='svhn_cropped', help='data set name = {mnist, svhn_cropped}')
 parser.add_argument('--dir_prefix', type=str, default='new_results_ss_', help='results directory prefix')
 args = parser.parse_args()
 
